dbf646d9d4dc6975cc0f72809da39916396eca4e
Carol-Student22 <carol.lian22@gmail.com>
2021-10-18 12:27:34 -0400

    .unfinished

---
+++ project/src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="main-shop">
+        <div class="header">Welcome Hello Greetings {{msg}} </div>
+
+        <div class="row counter">
+            <h1 class="text"></h1>
+            <div class="column product">
+                <button class="btn"></button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props: {
+        msg:String,
+    }
+}
+</script>
+
+<style >
+.main-shop {
+    
+}
+</style>

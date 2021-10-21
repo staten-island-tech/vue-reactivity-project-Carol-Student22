c79b735d2962f943fef9211c77b43a5b297aa6ec
Carol-Student22 <carol.lian22@gmail.com>
2021-10-21 12:25:59 -0400

    .

--- router-project/src/views/Store.vue
+++ router-project/src/views/Store.vue
@@ -15,7 +15,7 @@
             </div>  <!-- End of "left-counter" -->
 
             <div class="right-counter">
-                <button class="cost"> {{ product.cost }} </button>
+                <button class="product-cost"> {{ product.cost }} </button>
             </div>  <!-- End of "right-counter" -->
 
         </section>  <!-- End of "bottom-col column" -->
@@ -33,6 +33,7 @@ export default {
         return {
             product: {
                 name:"Bee" , cost: 100,
+                
             } // End of product
         } // End of return
     } // End of data
@@ -51,12 +52,11 @@ export default {
 }
 
 .main-shop {
-    background: grey; 
     min-width: 35rem;
     max-width: 60rem;
 
-    padding: 1rem 0 1rem 0;
-    border: rgb(202, 202, 202) 7px solid;
+    border: rgb(202, 202, 202) 3px solid;
+    margin: 2rem 0 0 0 ;
 }
 
 .top-col {
@@ -65,13 +65,13 @@ export default {
 .bottom-col {
     width: 100%;
 
-    border-top: white solid 1px;
+    border-top: white solid 2px;
 }
 
 .left-counter {
     width: 66.66%;
 
-    border-right: white 1px solid;
+    border-right: gray 2px solid;
     padding: 0.5rem; 
 }
 .right-counter {
@@ -79,7 +79,13 @@ export default {
     padding: 0.5rem;
 }
 
-.product-name:hover {
+.product-cost,
+.product-name {
+    border: none;
+}
+.product-name:hover, 
+.product-cost:hover  {
   cursor: pointer;
+  text-decoration: white 1.5px underline;
 }
 </style>
--- router-project/src/views/User.vue
+++ router-project/src/views/User.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="container">
-      <input type="text" placeholder="Name">
-      
+      <div class="name-container">
+        <input type="text" placeholder="Game Name" class="input">
+        <button class="input-btn">Change Name</button>
+      </div>
   </section>
 </template>
 
@@ -12,5 +14,36 @@ export default {
 </script>
 
 <style>
+.container {
+  display: flex;
+  justify-content: center;
+
+
+}
+.name-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+
+.input {
+  width: 15rem;
+  margin-bottom: 1rem;
+  outline: none;
+  border: solid 2px white;
+  padding: 5px;
+}
+
+
+.input-btn {
+  width: 9rem;
+  text-decoration: none;
+  border: 1.5px white solid;
+}
+.input-btn:hover {
+    box-shadow: none;
+    color: grey;
+  }
 
 </style>
